b6f2e1345424dc05960442024039e583f9445da2
Dre-Drey <51761267+Dre-Drey@users.noreply.github.com>
2023-01-13 10:53:29 +0100

    maj chronologie projets

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-// function hideNoRecherche() {
-//   document.querySelectorAll(".recherche").forEach((nodeElem) => {
-//     nodeElem.style.display = "flex";
-//   });
-//   document.querySelectorAll(".code").forEach((nodeElem) => {
-//     nodeElem.style.display = "none";
-//   });
-//   document.querySelectorAll(".musees").forEach((nodeElem) => {
-//     nodeElem.style.display = "none";
-//   });
-// }
-
-function toggleCategory(category) {
-  //toggle ma categorie
-  document.querySelectorAll(category).forEach((nodeElem) => {
+function hideNoRecherche() {
+  document.querySelectorAll(".recherche").forEach((nodeElem) => {
     nodeElem.style.display = "flex";
   });
-  //hide other categories
-  document.querySelectorAll(getNonCategories(category)).forEach((nodeElem) => {
+  document.querySelectorAll(".code").forEach((nodeElem) => {
+    nodeElem.style.display = "none";
+  });
+  document.querySelectorAll(".musees").forEach((nodeElem) => {
     nodeElem.style.display = "none";
   });
 }
 
-function getNonCategories(category) {
-  const globalSet = new Set();
-  // get all categories
-  document.querySelectorAll(".Projet").forEach((htmlElement) => {
-    Array.from(htmlElement.classList).forEach((classe) =>
-      globalSet.add(classe)
-    );
-  });
-  console.log(globalSet);
-  return Array.from(globalSet.delete(category));
-}
+// DEBUT REFACTO
+// function toggleCategory(category) {
+//   //toggle ma categorie
+//   document.querySelectorAll(category).forEach((nodeElem) => {
+//     nodeElem.style.display = "flex";
+//   });
+//   //hide other categories
+//   document.querySelectorAll(getNonCategories(category)).forEach((nodeElem) => {
+//     nodeElem.style.display = "none";
+//   });
+// }
+
+// function getNonCategories(category) {
+//   const globalSet = new Set();
+//   // get all categories
+//   document.querySelectorAll(".Projet").forEach((htmlElement) => {
+//     Array.from(htmlElement.classList).forEach((classe) =>
+//       globalSet.add(classe)
+//     );
+//   });
+//   console.log(globalSet);
+//   return Array.from(globalSet.delete(category));
+// }
 
 function hideNoCode() {
   document.querySelectorAll(".code").forEach((nodeElem) => {
